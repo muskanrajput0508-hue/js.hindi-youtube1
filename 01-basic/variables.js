8821fe178f62866ab25b23c0fbc9aa15633aa012
muskan rajput <muskanrajput0508@gmail.com>
2026-01-12 11:55:23 +0000

    muskan

---
+++ 01-basic/variables.js
@@ -0,0 +1,23 @@
+const accountid = 14435
+let  accountEmail = "muskanrajput0508@gmail.com"
+var accountpassword = "123456"
+accountcity = "jaipur"
+let accountstate;
+
+// accountid = 2// not allowed
+
+accountEmail= "shivrai@gmail.com"
+accountpassword = "21456"
+accountcity = "Bengalur"
+
+
+/*
+prefer not use to Var 
+because of issue in block scope and function scope
+*/
+
+console.log(accountid);
+
+console.table([ accountid , accountEmail , accountpassword , accountcity , accountstate])
+
+
